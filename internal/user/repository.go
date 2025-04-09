24c3b8e995bb28f6e17e74f7ec73bb58e772d0aa
Babahasko <zakutni4ek@gmail.com>
2025-04-09 23:08:25 +0300

    add user repository methods

--- internal/user/repository.go
+++ internal/user/repository.go
@@ -14,3 +14,27 @@ func NewUserRepository(database *db.DB) *UserRepository {
 	}
 }
 
+func (repo *UserRepository) Create(user *User) (*User, error) {
+	result := repo.Database.DB.Create(user)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return user, nil
+}
+
+func (repo *UserRepository) GetByEmail(email string) (*User, error) {
+	var user User
+	result := repo.Database.DB.First(&user, "email = ?", email)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return &user, nil
+}
+
+func (repo *UserRepository) Delete(id uint) error {
+	result := repo.Database.DB.Delete(&User{},id)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
