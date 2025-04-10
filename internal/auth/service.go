fec6fa78f1b3b432b146e36e4ce34d411ad60cce
Babahasko <zakutni4ek@gmail.com>
2025-04-10 23:41:12 +0300

    update auth service

--- internal/auth/service.go
+++ internal/auth/service.go
@@ -1,6 +1,9 @@
 package auth
 
 import (
+	"errors"
+
+	"github.com/Babahasko/go-jwt-auth/internal/user"
 	"github.com/Babahasko/go-jwt-auth/pkg/di"
 )
 
@@ -12,4 +15,30 @@ func NewAuthService(userRepository di.IUserRepository) *AuthService {
 	return &AuthService{
 		UserRepository: userRepository,
 	}
+}
+
+func (service *AuthService) Register(email, password, name string) (string, error) {
+	existed_user, err := service.UserRepository.GetByEmail(email)
+
+	if err != nil {
+		return "", err
+	}
+
+	if existed_user != nil {
+		return "", errors.New(ErrorUserExist)
+	}
+	
+	user := &user.User{
+		Email:    email,
+		Name:     name,
+		Password: "",
+	}
+
+	result, err := service.UserRepository.Create(user)
+
+	if err != nil {
+		return "", err
+	}
+
+	return result.Email, nil
 }
