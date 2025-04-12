8fa6cb08ed99fe24acb77a43f699c0fec7d598fb
Babahasko <zakutni4ek@gmail.com>
2025-04-12 18:29:24 +0300

    update comments

--- pkg/middleware/cors.go
+++ pkg/middleware/cors.go
@@ -10,7 +10,7 @@ func CORS(next http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 	
-		// Обработка preflight запросов
+		// fetch preflight
 		if r.Method == http.MethodOptions {
 		  w.WriteHeader(http.StatusNoContent)
 		  return
