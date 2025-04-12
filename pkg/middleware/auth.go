4b150240ed661c454b0ec674d73601da5be6f80c
Babahasko <zakutni4ek@gmail.com>
2025-04-12 13:25:31 +0300

    handle jwt in Auth

--- pkg/middleware/auth.go
+++ pkg/middleware/auth.go
@@ -4,13 +4,21 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
+
+	"github.com/Babahasko/go-jwt-auth/configs"
+	"github.com/Babahasko/go-jwt-auth/pkg/jwt"
 )
 
-func IsAuthed(next http.Handler) http.Handler {
+func IsAuthed(next http.Handler, conf *configs.Config) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		authHeader := r.Header.Get("Authorization")
 		token := strings.TrimPrefix(authHeader, "Bearer ")
-		fmt.Println(token)
+		payload, err := jwt.NewJWT(conf.Auth.PrivateKeyFile, conf.Auth.PublicKeyFile).Parse(token)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusUnauthorized)
+			return
+		}
+		fmt.Println(payload)
 		next.ServeHTTP(w, r)
 	})
 }
